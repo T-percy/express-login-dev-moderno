61250749557e6073218a938cc74659e3695f826c
tpercy <tonnypercy@gmail.com>
2022-07-30 11:57:29 -0500

    configurando introcuccion para probar ruta basica con navegador

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import express from 'express';
 const app = express();
 
+app.get('/', (req, res) => {
+    res.json({ok:true})
+});
+
 app.listen(5000, () => {
-    console.log('🔥🔥🔥');
-})
+    console.log('🔥🔥🔥 http://localhost:5000');
+});
